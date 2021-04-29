93c0fdd298faf5d20835cb928e6d7706d8cf9ccf
Maus_nc17 <valscatty@gmail.com>
2021-04-28 21:37:57 -0500

    fuck this shit

--- google/search/views.py
+++ google/search/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
+import csv
+import requests
+import os, pathlib
+from datetime import datetime
+from elasticsearch import Elasticsearch, helpers
+
+es = Elasticsearch(hosts=["localhost:9200"])
 
 # Create your views here.
 def index(request):
+    load_data()
     return render(request, 'index.html')
 
 def search(request):
-    if request.method == 'POST':
-        search = request.POST['search']
+    print(es.indices.get_alias("*"))
         
     return render(request, 'search.html')
+
+def load_data():
+    with open ("search/lyrics.csv", 'r') as file:
+        reader = csv.DictReader(file)
+        helpers.bulk(es, reader, index="index_name")
+        
+    
+
+    
